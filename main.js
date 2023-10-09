5d8b7dd68229a00f29d570701513e0bfbbbfb873
CatHat <liase810@gmail.com>
2023-10-09 16:06:02 +0200

    Added p5.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-"use strict";
+//"use strict";
 //import { characters } from "./characters/characters.js";
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-context.drawImage(getHTMLImage('./characters/images/ana.png'), 0, 0);
 
-function getHTMLImage(src) {
-    let img = document.createElement('img');
-    img.src = src;
-    return img;
+function setup() {
+    createCanvas(256*3, 256);
+}
+
+function draw() {
+    background(150);
 }
